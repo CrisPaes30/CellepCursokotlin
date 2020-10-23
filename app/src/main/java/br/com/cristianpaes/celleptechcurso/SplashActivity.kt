0c6291157ba7cfd0dfb0867fcdad6a843509d5d8
CrisPaes30 <cristianpaes90@gmail.com>
2020-10-22 22:55:03 -0300

    Programando Splash

--- app/src/main/java/br/com/cristianpaes/celleptechcurso/SplashActivity.kt
+++ app/src/main/java/br/com/cristianpaes/celleptechcurso/SplashActivity.kt
@@ -1,11 +1,27 @@
 package br.com.cristianpaes.celleptechcurso
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Abrir a tela de LoginActivty após esperar 2.5 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            //Criando uma intent e guardando ela numa variável
+            val mIntent = Intent(this, LoginActivity::class.java )
+
+            //Iniciar a Intent
+            startActivity(mIntent)
+
+            //Finalizando a SplashActivity
+            finish()
+        }, 2500)
     }
 }
